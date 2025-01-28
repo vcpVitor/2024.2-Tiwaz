a2ab8e2fbab3664e15d842272c49f38c119c61a5
JoaoPedro2206 <jpfmoraes@hotmail.com>
2025-01-28 16:51:57 -0300

    CRUD DE CUSTOS

--- CultivaTech/ReadMe.md
+++ CultivaTech/ReadMe.md
@@ -1,5 +1,6 @@
 # Bibliotecas
 ## Front End
+- npm install expo
 - npm install axios
 - npm install @react-navigation/native @react-navigation/stack
 - npm install react-native-screens react-native-safe-area-context react-native-gesture-handler react-native-reanimated react-native-vector-icons
---
+++ CultivaTech/backend/src/modules/custos/controllers/custosController.js
@@ -0,0 +1,97 @@
+const custosService = require('../services/custosService');
+
+// Cadastrar Plantação
+const cadastrarCusto = async (req, res) => {
+  try {
+    const { nome, data, tipoCusto, descricao, valorCusto } = req.body;
+
+    // Validação dos campos obrigatórios
+    if (!nome || !data || !tipoCusto || !descricao || !valorCusto) {
+      return res.status(400).json({ error: 'Todos os campos obrigatórios devem ser preenchidos!' });
+    }
+
+    // Chama o serviço para cadastrar o custo
+    const novoCusto = await custosService.cadastrarCusto({
+      nome,
+      data,
+      tipoCusto,
+      descricao,
+      valorCusto,
+    });
+
+    res.status(201).json({
+      message: 'Custo cadastrado com sucesso!',
+      custo: novoCusto,
+    });
+  } catch (error) {
+    console.error('Erro ao cadastrar o custo:', error.message);
+    res.status(500).json({ error: 'Erro ao cadastrar o custo!' });
+  }
+};
+
+// Listar Custos
+const listarCustos = async (req, res) => {
+  try {
+    // Chama o serviço para listar todas os custos
+    const custos = await custosService.listarCustos();
+    res.status(200).json(custos);
+  } catch (error) {
+    console.error('Erro ao listar os custos:', error.message);
+    res.status(500).json({ error: 'Erro ao listar os custos!' });
+  }
+};
+
+// Atualizar Custo
+const atualizarCusto = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { nome, data, tipoCusto, descricao, valorCusto } = req.body;
+
+    // Chama o serviço para atualizar a plantação
+    const custoAtualizado = await custosService.atualizarCustos(id, {
+      nome,
+      data,
+      tipoCusto,
+      descricao,
+      valorCusto,
+    });
+
+    if (!custoAtualizado) {
+      return res.status(404).json({ error: 'Custo não encontrado!' });
+    }
+
+    res.status(200).json({
+      message: 'Custo atualizado com sucesso!',
+      custo: custoAtualizado,
+    });
+  } catch (error) {
+    console.error('Erro ao atualizar o custo:', error.message);
+    res.status(500).json({ error: 'Erro ao atualizar o custo!' });
+  }
+};
+
+// Excluir Custo
+const excluirCusto = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    // Chama o serviço para excluir o custo
+    const custoExcluido = await custosService.excluirCusto(id);
+
+    if (!custoExcluido) {
+      return res.status(404).json({ error: 'Custo não encontrado!' });
+    }
+
+    res.status(200).json({ message: 'Custo excluído com sucesso!' });
+  } catch (error) {
+    console.error('Erro ao excluir o custo:', error.message);
+    res.status(500).json({ error: 'Erro ao excluir custo!' });
+  }
+};
+
+module.exports = {
+    cadastrarCusto,
+    listarCustos,
+    atualizarCusto,
+    excluirCusto,
+};
